0438f595b77b1c4b70161c9f18ee4d3261e1a893
John Paul Quintana <jpquintana2024@gmail.com>
2024-05-01 13:54:41 +0800

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
     </style>
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
