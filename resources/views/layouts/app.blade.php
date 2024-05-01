25cc1f29c83a2fb82526fc48b3344339876fd78e
PlebbyLord <127131122+PlebbyLord@users.noreply.github.com>
2024-05-01 14:25:47 +0800

    updated name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
             <div class="container">
                 <img src="{{ asset('storage/images/Bean-to-Brew.png') }}" alt="Logo" width="50" height="50">
                 <a class="navbar-brand" href="/">
-                    Bean to Brew i want to change this
+                    Bean to Brew
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
