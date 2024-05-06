ba63127d9357c3a0a1c937b5d1dd73b77f48b09b
PlebbyLord <127131122+PlebbyLord@users.noreply.github.com>
2024-05-07 02:49:11 +0800

    Update ScheduleController.php

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -40,20 +40,15 @@ class ScheduleController extends Controller
     {
         return view('Features.Schedules.history');
     }
-    public function history1(Request $request)
+
+    public function history1()
     {
-        // Validate the request
-        $request->validate([
-            'schedule_type' => 'required|in:Planting,Watering,MonthlyChecks,Pesticide Spraying,Harvesting,Pulping,Fermenting,Drying,Hulling,Sorting,Pruning,Packaging,Roasting,Grinding'
-        ]);
-    
-        // Retrieve schedules based on the provided schedule type and location 'Farm 1'
+        // Retrieve schedules where progress_status is 0 or 1 and location is 'Farm 1'
         $schedules = Schedule::whereIn('progress_status', [0, 1])
                               ->where('location', 'Farm 1')
-                              ->where('Schedule_Type', $request->schedule_type)
                               ->get();
         
-        // Pass the data to the view
+        // Pass the data to the view and return it
         return view('Features.Schedules.History.history1', compact('schedules'));
     }
 
