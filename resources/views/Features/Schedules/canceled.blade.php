4e940ebb8c0235e390cc1e826c6e6d16ad759bcf
PlebbyLord <127131122+PlebbyLord@users.noreply.github.com>
2024-05-07 14:20:43 +0800

    Update canceled.blade.php

--- resources/views/Features/Schedules/canceled.blade.php
+++ resources/views/Features/Schedules/canceled.blade.php
@@ -12,16 +12,16 @@
                 <div class="col-md-12">
                     <div class="row">
                         <div class="col-md-6 mb-2 mx-auto text-center">
-                            <a href="{{ route('canceled1') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 50px;">Canceled Farm 1 Schedules</a>
+                            <a href="{{ route('canceled1') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 70px;">Canceled Farm 1 Schedules</a>
                         </div>
                         <div class="col-md-6 mb-2 mx-auto text-center">
-                            <a href="{{ route('canceled2') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 50px;">Canceled Farm 2 Schedules</a>
+                            <a href="{{ route('canceled2') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 70px;">Canceled Farm 2 Schedules</a>
                         </div>
                         <div class="col-md-6 mb-2 mx-auto text-center">
-                            <a href="{{ route('canceled3') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 50px;">Canceled Farm 3 Schedules</a>
+                            <a href="{{ route('canceled3') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 70px;">Canceled Farm 3 Schedules</a>
                         </div>
                         <div class="col-md-6 mb-2 mx-auto text-center">
-                            <a href="{{ route('canceled4') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 50px;">Canceled Farm 4 Schedules</a>
+                            <a href="{{ route('canceled4') }}" class="btn btn-primary btn-lg btn-block" style="width: 250px; height: 70px;">Canceled Farm 4 Schedules</a>
                         </div>
                     </div>
                 </div>
