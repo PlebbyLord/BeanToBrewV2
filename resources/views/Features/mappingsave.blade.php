bd039952e37df95a212a857f1064530910e7c2e5
PlebbyLord <127131122+PlebbyLord@users.noreply.github.com>
2024-05-09 12:28:02 +0800

    Update mappingsave.blade.php

--- resources/views/Features/mappingsave.blade.php
+++ resources/views/Features/mappingsave.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
+@if(session('error'))
+<div style="background-color: black; color: red; display: flex; justify-content: center; padding: 20px; font-size: 20px;">
+    {{ session('error') }}
+</div>
+@endif
+
+@if(session('success'))
+<div style="background-color: black; color: green; display: flex; justify-content: center; padding: 20px; font-size: 20px;">
+    {{ session('success') }}
+</div>
+@endif
+
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
